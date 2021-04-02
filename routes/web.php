176a6cbc34b3f6215110a236c67931b158b22d5d
gintokisakamoto12 <78606327+gintokisakamoto12@users.noreply.github.com>
2021-04-02 16:37:54 -0500

    correle

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('admin/home', 'HomeController@adminHome')->name('admin.home')->middleware('is_admin');
+
+
+
+
+//lees esto?
