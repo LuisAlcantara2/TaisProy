3c8360c199f3d2c33d4b4a283aaf472699be6feb
LuisAlcantara2 <77132799+LuisAlcantara2@users.noreply.github.com>
2021-04-19 17:43:03 -0500

    Plan A

--- resources/views/tablas/estrategias/mapa.blade.php
+++ resources/views/tablas/estrategias/mapa.blade.php
@@ -1,4 +1,55 @@
 @extends('layouts.plantilla')
+@section('estilos')
+<style>
+    table {
+		/* Para centrar usamos "auto" en la alineación horizontal */
+    margin: 0 auto;
+		/*Lo siguiente es para los bordes*/
+    border-collapse: collapse;
+    }
+    .table img{
+        width: 50px;
+        margin-left: 10%;
+        margin-right: 10%;
+    }
+    .table .container-img-arriba{
+        text-align: center;
+    }
+    .financiera{
+        background-color: green;
+        width: 100%;
+        height: 50px;
+        border: 1px solid;
+        text-align: center;
+    }
+    .cliente{
+        background-color: yellow;
+        width: 100%;
+        height: 50px;
+        border: 1px solid;
+        text-align: center;
+    }
+    .negocio{
+        background-color: orange;
+        width: 100%;
+        height: 50px;
+        border: 1px solid;
+        text-align: center;
+    }
+    .aprendizaje{
+        background-color: blue;
+        width: 100%;
+        height: 50px;
+        border: 1px solid;
+        text-align: center;
+    }
+    .caja{
+        border: 1px solid;
+        text-align: center;
+        padding: 6px;
+    }
+</style>
+@endsection
 @section('contenido')
 <a href="{{route('proceso.estrategia',$id)}}" class="btn btn-info"><i class="fas fa-arrow-left"></i> Volver</a>
 <div class="row">
@@ -19,76 +70,95 @@
                 <input type="hidden" id="idEditar" value="">
             </div>
             <div class="card-block">
-                <div class="col-10 offset-1">
+                <div style="text-align:center;">
                     <table class="table">
                         <tbody>
                             <tr>
                                 <td>
-                                    <div>
-                                        <h3 style="font-weight: bold;">FINANCIERA</h3>
+                                    <div class="financiera">
+                                        <p style="font-weight: bold;">FINANCIERA</p>
                                     </div>
                                 </td>
-
+                                @foreach($estrategia as $itemestrategia)
+                                @if($itemestrategia->tipo==0)
                                 <td>
-                                    <div>
-                                        @foreach($estrategia as $itemestrategia)
-                                        @if($itemestrategia->tipo==0)
-                                        <p>{{$itemestrategia->descripcion}}</p>
-                                        @endif
-                                        @endforeach
+                                    <div class="caja">
+                                        {{$itemestrategia->descripcion}}
+                                    </div>
+                                </td>
+                                @endif
+                                @endforeach
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="container-img-arriba">
+                                        <img src="/images/balance-flecha-arriba.png" alt="">
                                     </div>
                                 </td>
                             </tr>
                             <tr>
                                 <td>
-                                    <div>
-                                        <h3 style="font-weight: bold;">CLIENTE</h3>
+                                    <div class="cliente">
+                                        <p style="font-weight: bold;">CLIENTE</p>
                                     </div>
                                 </td>
-
+                                @foreach($estrategia as $itemestrategia)
+                                @if($itemestrategia->tipo==1)
                                 <td>
-                                    <div>
-                                        @foreach($estrategia as $itemestrategia)
-                                        @if($itemestrategia->tipo==1)
-                                        <p>{{$itemestrategia->descripcion}}</p>
-                                        @endif
-                                        @endforeach
+                                    <div class="caja">
+                                        {{$itemestrategia->descripcion}}
+                                    </div>
+                                </td>
+                                @endif
+                                @endforeach
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="container-img-arriba">
+                                        <img src="/images/balance-flecha-arriba.png" alt="">
                                     </div>
                                 </td>
                             </tr>
                             <tr>
                                 <td>
-                                    <div>
-                                        <h3 style="font-weight: bold;">PROCESO DE NEGOCIO</h3>
+                                    <div class="negocio">
+                                        <p style="font-weight: bold;">PROCESO DE NEGOCIO</p>
                                     </div>
                                 </td>
 
+                                @foreach($estrategia as $itemestrategia)
+                                @if($itemestrategia->tipo==2)
                                 <td>
-                                    <div>
-                                        @foreach($estrategia as $itemestrategia)
-                                        @if($itemestrategia->tipo==2)
-                                        <p>{{$itemestrategia->descripcion}}</p>
-                                        @endif
-                                        @endforeach
+                                    <div class="caja">
+                                        {{$itemestrategia->descripcion}}
+                                    </div>
+                                </td>
+                                @endif
+                                @endforeach
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="container-img-arriba">
+                                        <img src="/images/balance-flecha-arriba.png" alt="">
                                     </div>
                                 </td>
                             </tr>
                             <tr>
                                 <td>
-                                    <div>
-                                        <h3 style="font-weight: bold;">APRENDIZAJE Y CONOCIMIENTO</h3>
+                                    <div class="aprendizaje">
+                                        <p style="font-weight: bold;">APRENDIZAJE Y CONOCIMIENTO</p>
                                     </div>
                                 </td>
 
+                                @foreach($estrategia as $itemestrategia)
+                                @if($itemestrategia->tipo==3)
                                 <td>
-                                    <div>
-                                        @foreach($estrategia as $itemestrategia)
-                                        @if($itemestrategia->tipo==3)
-                                        <p>{{$itemestrategia->descripcion}}</p>
-                                        @endif
-                                        @endforeach
+                                    <div class="caja">
+                                        {{$itemestrategia->descripcion}}
                                     </div>
                                 </td>
+                                @endif
+                                @endforeach
                             </tr>
                         </tbody>
                     </table>
